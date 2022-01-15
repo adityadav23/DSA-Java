14da99c78b4c65c2ffcbbec69c6f539a2d5a3ad1
adityadav23 <72222769+adityadav23@users.noreply.github.com>
2022-01-15 12:26:16 +0530

    BubbleSort

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+  static void BubbleSort(int[] arr){
+      
+      int length = arr.length;
+     
+         for(int i =0; i< length; i++){
+             
+             boolean swapped = false;
+             for(int j =1; j< length-i ;j++){
+                 if(arr[j]< arr[j-1]){
+                     swap(arr, j, j-1);
+                 
+                     swapped = true;
+                 }
+                 
+             }
+             
+             if(!swapped){
+                 return ;
+             }
+         }
+     }
+     
+private static void swap(int[] arr, int i , int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+
