5ac6126655cc9da5adb474072f21ba4387b6057f
adityadav23 <72222769+adityadav23@users.noreply.github.com>
2022-01-18 16:52:44 +0530

    Update SkipString.java

---
+++ Stream/SkipString.java
@@ -0,0 +1,31 @@
+package Stream;
+
+public class Stream{
+
+    public static void main(String[] args){
+
+        String x ="qweapplexyz";
+        String ignore = "apple";
+      String ans =  skipString(x,ignore );
+      System.out.println(ans);
+    }
+
+  
+    
+    
+    static String skipString( String input, String ignoreString){
+        
+        if(input.isEmpty()){
+            return "" ;
+        }
+        if(input.startsWith(ignoreString)){
+
+           return skipString( input.substring(ignoreString.length()),ignoreString);
+        }
+        else{
+            return  input.charAt(0) + skipString( input.substring(1), ignoreString);
+        }
+    }
+
+    
+}
